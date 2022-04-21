0b5190848a67dfdc704a76f915e2508316a1ff88
rutherford-alexis <rutherford.aalexis@gmail.com>
2022-04-21 17:24:29 -0400

    yeah

--- index.js
+++ index.js
@@ -44,9 +44,23 @@ class Airplane {
 */
 
 class Person {
-  
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+  }
 }
-
+Person.prototype.eat = function(thing) {
+  if (this.stomach.length < 10){
+  this.stomach.push(thing);
+ }
+}
+Person.prototype.poop = function(){
+  this.stomach = [];
+}
+Person.prototype.toString = function(){
+  return `${this.name}, ${this.age}`;
+}
 /*
   TASK 2
     - Write a Car class whose constructor initializes `model` and `milesPerGallon`, from 2 arguments.
@@ -62,7 +76,27 @@ class Person {
 */
 
 class Car {
-  
+  constructor(model, milesPerGallon){
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
+  fill(gallons){
+    this.tank = this.tank + gallons;
+  }
+  drive(distance){
+   let juice= this.tank * this.milesPerGallon
+    if (juice >= distance){
+    this.odometer = this.odometer + distance;
+    this.tank = this.tank - (distance / this.milesPerGallon);
+    } else {
+      this.odometer= this.odometer + juice;
+      this.tank = 0
+      return `I ran out of fuel at ${this.odometer} miles!`;
+    }
+
+  }
 }
 
 /*
@@ -79,7 +113,14 @@ class Car {
 */
 
 class Lambdasian {
-  
+  constructor({name, age, location}){
+    this.name= name;
+    this.age= age;
+    this.location= location;
+  }
+  speak(){
+    return `Hello my name is ${this.name}, I am from ${this.location}`;
+  }
 }
 
 /*
@@ -97,8 +138,19 @@ class Lambdasian {
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
 
-class Instructor {
-
+class Instructor extends Lambdasian {
+  constructor({name, age, location, specialty, favLanguage, catchPhrase}){
+  super({name, age, location, specialty, favLanguage, catchPhrase})
+    this.favLanguage= favLanguage;
+    this.specialty= specialty;
+    this.catchPhrase= catchPhrase;
+  }
+  demo(subject){
+   return `Today we are learning about ${subject}`;
+  }
+  grade(student, subject){
+   return `${student.name} receives a perfect score on ${subject}`;
+  }
 }
 
 /*
@@ -117,9 +169,25 @@ class Instructor {
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
 
-class Student {
-   
+class Student extends Lambdasian {
+  constructor({name, age, location, previousBackground, className, favSubjects}){
+  super({name, age, location, previousBackground, className, favSubjects});
+   this.previousBackground = previousBackground;
+   this.className= className;
+   this.favSubjects= favSubjects;
+  }
+  listSubjects(){
+    return `${this.favSubjects}`
+  }
+  PRAssignment(subject){
+    return `${this.name} has submitted a PR for ${subject}`;
+  }
+  sprintChallenge(subject){
+    return `${this.name} has begun sprint challenge on ${subject}`;
+  }
 }
+
+
 
 /*
   TASK 6
@@ -135,10 +203,19 @@ class Student {
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
 
-class ProjectManager {
-   
+class ProjectManager extends Instructor {
+  constructor({name, age, location, specialty, favLanguage, catchPhrase, gradClassName, favInstructor}){
+    super({name, age, location, specialty, favLanguage, catchPhrase, gradClassName, favInstructor});
+    this.gradClassName= gradClassName;
+    this.favInstructor= favInstructor;
 }
-
+standUp(channel){
+  return  `${this.name} announces to ${channel}, @${channel} standy times!`;
+}
+debugsCode(student, subject){
+  return `${this.name} debugs ${student.name}'s code on ${subject}`;
+}
+}
 /*
   STRETCH PROBLEM (no tests!)
     - Extend the functionality of the Student by adding a prop called grade and setting it equal to a number between 1-100.
@@ -147,8 +224,19 @@ class ProjectManager {
       + This method, when called, will check the grade of the student and see if they're ready to graduate from BloomTech
       + If the student's grade is above a 70% let them graduate! Otherwise go back to grading their assignments to increase their score.
 */
-
-
+class studentextnd extends Student{
+  constructor({name, age, location, previousBackground, className, favSubjects, grade}){
+    super({name, age, location, previousBackground, className, favSubjects, grade});
+    this.grade= grade;
+}
+graduate(){
+  if (this.grade >= 70){
+    return "LET THEM GRADUATE!";
+  }else {
+    return "go back to grading their assignments to increase their score, so they can graduate"
+  }
+}
+}
 //End of Challenge
 /* 🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑 Please do not modify anything below this line 🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑 */
 function foo(){
